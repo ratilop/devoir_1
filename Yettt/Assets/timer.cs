2534092e8f07f08a8cead4e270961214b85e0149
maxime <max_sur11@outlook.com>
2019-10-01 22:41:58 -0400

    hub four player

---
+++ Yettt/Assets/timer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    [SerializeField] private Text uiText;
+    [SerializeField] private float mainTimer;
+
+    // Start is called before the first frame update
+    private float Timer;
+    private bool canCount = true;
+    private bool doOnce = false;
+    void Start()
+    {
+        Timer = mainTimer;
+    }
+
+
+        // Update is called once per frame
+        void Update()
+    {
+        if(Timer >= 0.0f && canCount)
+        {
+            Timer -= Time.deltaTime;
+            uiText.text = Timer.ToString("F");
+        }
+        else if(Timer <=0 && !doOnce)
+        {
+            canCount = false;
+            doOnce = true;
+            uiText.text = "0.00";
+            Timer = 0.0f;
+
+        }
+    }
+}
